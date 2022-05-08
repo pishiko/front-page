d55a8d93924bd298be4c2ee4ab97259f5d8e9464
pishiko <55dorakoro@gmail.com>
2022-05-09 04:43:25 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
+  const [titleActive, setTitleActive] = useState(false);
+  const [explosion, setExplosion] = useState(false);
+  const [curtain, setCurtain] = useState(false);
+
+  useEffect(() => {
+    setTitleActive(true);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <CSSTransition
+        in={titleActive}
+        timeout={2000}
+        unmountOnExit={false}
+        classNames={"titles"}
+        onEntered={() => {
+          setTitleActive(false);
+          new Promise((resolve) => setTimeout(resolve, 4700)).then(() =>
+            setCurtain(true)
+          );
+        }}
+        onExited={() => setExplosion(true)}
+      >
+        <div className="titles">
+          <div className="title">p</div>
+          <div className="title">4</div>
+          <div className="title">k</div>
+          <CSSTransition
+            in={explosion}
+            timeout={3000}
+            unmountOnExit={false}
+            classNames={"tomato"}
+            onEntered={() => setExplosion(false)}
+          >
+            <div className="tomato"></div>
+          </CSSTransition>
+          <div className="title">.</div>
+          <div className="title">c</div>
+          <div className="title">o</div>
+          <div className="title">m</div>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={curtain}
+        timeout={1000}
+        unmountOnExit={false}
+        classNames={"content"}
+      >
+        <div
+          className="content"
+          style={{ fontSize: "10vh", color: "white", paddingLeft: "3vw" }}
         >
-          Learn React
-        </a>
-      </header>
+          p4ko.com
+          <div style={{ fontSize: "3vh", color: "white" }}>
+            <div>
+              <a
+                target={"_blank"}
+                href="https://twitter.com/pishitaro_"
+                rel="noreferrer"
+              >
+                Twitter (@pishitaro_)
+              </a>
+            </div>
+            <div>
+              <a
+                target={"_blank"}
+                href="https://memo.p4ko.com"
+                rel="noreferrer"
+              >
+                TechBlog (memo.p4ko.com)
+              </a>
+            </div>
+            <div>
+              <a target={"_blank"} href="https://note.com/p4k" rel="noreferrer">
+                note (note.com/p4k)
+              </a>
+            </div>
+            <div>
+              <a
+                target={"_blank"}
+                href="https://github.com/pishiko"
+                rel="noreferrer"
+              >
+                GitHub (github.com/pishiko)
+              </a>
+            </div>
+          </div>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
